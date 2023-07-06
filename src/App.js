2e8734f43dac9d6f9275b1eb0dada0fff8d8abe1
nhillson <nicholas.hillson@gmail.com>
2023-07-06 14:53:54 -0500

    Icons changed, pages beginning to form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import { BrowserRouter, Route, Routes, Link, } from "react-router-dom"
 
+function LangItem({ lang }) {
+  let key = Object.keys(lang)[0];
+  let value = lang[0];
+  return (
+    <tr>
+      <h4>{ key }</h4>
+      <h6>{ value }</h6>
+    </tr>
+  );
+}
+
+function LangList() {
+  const langs = [{JS: "I learned in xxx"}];
+  const rows = [];
+  langs.forEach((lang) => {
+    rows.push(<LangItem lang={lang} />)
+  })
+  return (
+    <div>{ rows }</div>
+  );
+}
+
 function Homepage() {
   return (
     <div>
-      Homepage
+      <h2 className="title">
+        Hello, and welcome to my website! This site contains several personal projects as well as information about myself, and will be update over time to reflect my continuing education.
+      </h2>
     </div>
   );
 }
@@ -11,7 +35,7 @@ function Homepage() {
 function Languages() {
   return (
     <div>
-      Languages
+      <LangList />
     </div>
   )
 }
@@ -27,19 +51,20 @@ function NotFoundPage() {
 function App() {
   return (
     <BrowserRouter>
-    <header>
+    <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/languages" element={<Languages />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    <footer>
       <nav>
         <ul>
           <li><Link to="/">Homepage</Link></li>
           <li><Link to="/languages">Languages</Link></li>
         </ul>
       </nav>
-    </header>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/languages" element={<Languages />} />
-        <Route path="/*" element={<NotFoundPage />} />
-      </Routes>
+    </footer>
+      
     </BrowserRouter>
     
   );
