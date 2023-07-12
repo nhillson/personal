78f509784da3793133b81da007e30d08bce5cbb7
nhillson <nicholas.hillson@gmail.com>
2023-07-12 09:57:35 -0500

    Added click handling for language TD

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom"
+import { BrowserRouter, Link, Route, Routes} from "react-router-dom"
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
+import { useState } from 'react';
 
+const langs = [{language:"C", resources:["Harvard's CS50X"]}, {language:"Python 3", resources:["Python Crash Course", "Harvard's CS50X", "Harvard's CS50P", "Code Combat", "The Big Book of Small Python Projects"]}, {language:"HTML/CSS", resources:["Harvard's CS50X", "Harvard's CS50W", "FreeCodeCamp"]}, {language:"JavaScript", resources:["Harvard's CS50W", "Mozilla Developer Network", "Code Combat"]}, {language:"SQL", resources:["Harvard's CS50X"]}, {language:"Django", resources:["Harvard's CS50W"]}, {language:"React", resources:["Harvard's CS50W", "React Documentation"]}];
 
 function NavBar() {
   return (
     <Navbar>
       <Navbar.Brand><img alt="Cute Mushroom" src="brand.png" width="40" height="40" /></Navbar.Brand>
       <Nav>
-        <Nav.Link href="/">Homepage</Nav.Link>
-        <Nav.Link href="/languages">Languages</Nav.Link>
+        <LinkContainer to="/"><Nav.Link>Homepage</Nav.Link></LinkContainer>
+        <LinkContainer to="/languages"><Nav.Link>Languages</Nav.Link></LinkContainer>
+        <LinkContainer to="/about"><Nav.Link>About Me</Nav.Link></LinkContainer>
       </Nav>
     </Navbar>
   );
@@ -20,9 +24,12 @@ function NavBar() {
 function LangItem({ lang }) {
   let language = lang.language;
   let resources = lang.resources.join(", ");
+  function handleClick() {
+    alert('clicked!');
+  }
   return (
     <tr>
-      <td>
+      <td onClick={handleClick}>
       <div className="table-lang"><b>{language}</b></div>
       <div className="table-resource">Resources:  {resources}</div>
       </td>
@@ -31,7 +38,7 @@ function LangItem({ lang }) {
 }
 
 function LangList() {
-  const langs = [{language:"C", resources:["Harvard's CS50X"]}, {language:"Python 3", resources:["Python Crash Course", "Harvard's CS50X", "Harvard's CS50P", "Code Combat", "The Big Book of Small Python Projects"]}, {language:"HTML/CSS", resources:["Harvard's CS50X", "Harvard's CS50W", "FreeCodeCamp"]}, {language:"JavaScript", resources:["Harvard's CS50W", "Mozilla Developer Network", "Code Combat"]}, {language:"SQL", resources:["Harvard's CS50X"]}, {language:"Django", resources:["Harvard's CS50W"]}, {language:"React", resources:["Harvard's CS50W", "React Documentation"]}];
+  
   const rows = [];
   langs.forEach((lang) => {
     rows.push(<LangItem lang={lang} />)
@@ -50,7 +57,6 @@ function LangList() {
 function Homepage() {
   return (
     <Container fluid>
-      <NavBar />
       <Row>
         <Col></Col>
         <Col xs={8}>
@@ -67,11 +73,24 @@ function Homepage() {
 function Languages() {
   return (
     <Container fluid>
-      <NavBar />
     
       <LangList />
     </Container>
   )
+}
+
+function About() {
+  return (
+    <Container fluid>
+      <Row>
+        <Col></Col>
+        <Col xs={8}>
+          <h2>Hi</h2>
+        </Col>
+        <Col></Col>
+      </Row>
+    </Container>
+  );
 }
 
 function NotFoundPage() {
@@ -85,11 +104,15 @@ function NotFoundPage() {
 function App() {
   return (
     <BrowserRouter>
+    <Container fluid>
+      <NavBar />
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/languages" element={<Languages />} />
+        <Route path="/about" element={<About />} />
         <Route path="/*" element={<NotFoundPage />} />
       </Routes>
+    </Container>
     </BrowserRouter>
   );
 }
