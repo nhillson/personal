057f5f83baa48ac930164b55a436742da5e1bae0
nhillson <nicholas.hillson@gmail.com>
2023-07-13 10:43:26 -0500

    minor tweaks

--- src/App.js
+++ src/App.js
@@ -1,55 +1,94 @@
-import { BrowserRouter, Route, Routes} from "react-router-dom"
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
-import LinkContainer from 'react-router-bootstrap/LinkContainer';
-import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import LinkContainer from "react-router-bootstrap/LinkContainer";
+import { useState } from "react";
 
-const langs = [{language:"C", resources:["Harvard's CS50X"]}, {language:"Python 3", resources:["Python Crash Course", "Harvard's CS50X", "Harvard's CS50P", "Code Combat", "The Big Book of Small Python Projects"]}, {language:"HTML/CSS", resources:["Harvard's CS50X", "Harvard's CS50W", "FreeCodeCamp"]}, {language:"JavaScript", resources:["Harvard's CS50W", "Mozilla Developer Network", "Code Combat"]}, {language:"SQL", resources:["Harvard's CS50X"]}, {language:"Django", resources:["Harvard's CS50W"]}, {language:"React", resources:["Harvard's CS50W", "React Documentation"]}];
+const langs = [
+  { language: "C", resources: ["Harvard's CS50X"] },
+  {
+    language: "Python 3",
+    resources: [
+      "Python Crash Course",
+      "Harvard's CS50X",
+      "Harvard's CS50P",
+      "Code Combat",
+      "The Big Book of Small Python Projects",
+    ],
+  },
+  {
+    language: "HTML/CSS",
+    resources: ["Harvard's CS50X", "Harvard's CS50W", "FreeCodeCamp"],
+  },
+  {
+    language: "JavaScript",
+    resources: ["Harvard's CS50W", "Mozilla Developer Network", "Code Combat"],
+  },
+  { language: "SQL", resources: ["Harvard's CS50X"] },
+  { language: "Django", resources: ["Harvard's CS50W"] },
+  { language: "React", resources: ["Harvard's CS50W", "React Documentation"] },
+];
 
 function NavBar() {
   return (
     <Navbar>
-      <Navbar.Brand><img alt="Cute Mushroom" src="brand.png" width="40" height="40" /></Navbar.Brand>
+      <Navbar.Brand>
+        <img alt="Cute Mushroom" src="brand.png" width="40" height="40" />
+      </Navbar.Brand>
       <Nav>
-        <LinkContainer to="/"><Nav.Link>Homepage</Nav.Link></LinkContainer>
-        <LinkContainer to="/languages"><Nav.Link>Languages</Nav.Link></LinkContainer>
-        <LinkContainer to="/about"><Nav.Link>About Me</Nav.Link></LinkContainer>
+        <LinkContainer to="/">
+          <Nav.Link>Homepage</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/languages">
+          <Nav.Link>Languages</Nav.Link>
+        </LinkContainer>
+        <LinkContainer to="/about">
+          <Nav.Link>About Me</Nav.Link>
+        </LinkContainer>
       </Nav>
     </Navbar>
   );
 }
-function LangItem({ lang , active, onShow }) {
+function LangItem({ lang, activeRow, onShow }) {
   let language = lang.language;
   let resources = lang.resources.join(", ");
   return (
     <tr onClick={onShow}>
       <td>
-      <div className="table-lang"><b>{language}</b></div>
-      {active === language ? (
-        <div className="table-resource">Resources:  {resources}</div>
-      ) : (
-        <div></div>
-      )}
+        <div className="table-lang">
+          <b>{language}</b>
+        </div>
+        {activeRow === language ? (
+          <div className="table-resource">Resources: {resources}</div>
+        ) : (
+          <div></div>
+        )}
       </td>
     </tr>
   );
 }
 
 function LangList() {
-  const [active, setActive] = useState("None");
+  const [activeRow, setActiveRow] = useState("None");
   const rows = [];
   langs.forEach((lang) => {
-    rows.push(<LangItem lang={lang} active={active} onShow={() => {
-      if (lang.language === active) {
-        setActive("None")
-      } else {
-        setActive(lang.language)
-      }
-    }} />)
-  })
+    rows.push(
+      <LangItem
+        lang={lang}
+        activeRow={activeRow}
+        onShow={() => {
+          if (lang.language === activeRow) {
+            setActiveRow("None");
+          } else {
+            setActiveRow(lang.language);
+          }
+        }}
+      />
+    );
+  });
   return (
     <table className="langtable">
       <thead>
@@ -57,7 +96,6 @@ function LangList() {
       </thead>
       <tbody>{rows}</tbody>
     </table>
-    
   );
 }
 
@@ -65,13 +103,15 @@ function Homepage() {
   return (
     <Container fluid>
       <Row>
-        <Col></Col>
+        <Col />
         <Col xs={8}>
           <h2 className="title">
-          Hello, and welcome to my website! This site contains several personal projects as well as a bit of information about myself, and will be updated over time to reflect my continuing education.
+            Hello, and welcome to my website! This site contains several
+            personal projects as well as a bit of information about myself, and
+            will be updated over time to reflect my continuing education.
           </h2>
         </Col>
-        <Col></Col>
+        <Col />
       </Row>
     </Container>
   );
@@ -80,10 +120,9 @@ function Homepage() {
 function Languages() {
   return (
     <Container fluid>
-    
       <LangList />
     </Container>
-  )
+  );
 }
 
 function About() {
@@ -101,25 +140,21 @@ function About() {
 }
 
 function NotFoundPage() {
-  return (
-    <div>
-      Page Not Found
-    </div>
-  )
+  return <div>Page Not Found</div>;
 }
 
 function App() {
   return (
     <BrowserRouter>
-    <Container fluid>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/languages" element={<Languages />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/*" element={<NotFoundPage />} />
-      </Routes>
-    </Container>
+      <Container fluid>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/languages" element={<Languages />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
